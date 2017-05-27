b3b69f52207d3a8111421ad7ab9ed82abbe85316
rocky <rocky@gnu.org>
2017-05-27 11:32:19 -0400

    Bump version

--- test-simple.el
+++ test-simple.el
@@ -7,7 +7,7 @@
 ;; URL: http://github.com/rocky/emacs-test-simple
 ;; Keywords: unit-test
 ;; Package-Requires: ((cl-lib "0"))
-;; Version: 1.2.0
+;; Version: 1.3.0
 
 ;; This program is free software: you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
